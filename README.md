fae4baf0a79069bc3d52f5e79b89fa74b44e7053
Timothy Oyetayo <toyetayo@academic.rrc.ca>
2024-05-20 12:56:28 -0500

    Ghost ruby script and updated README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# ** Carl Sagan Ruby Script **
+
+The script defines two main data structures:
+
+1. A hash `carl` with keys `:toast`, `:punctuation`, and `:words`.
+2. An array `sagan` containing various elements, including hashes and a string.
+
+
+# ** Ghosts Ruby Script **
+This Ruby script demonstrates how to create an array of hashes and iterate through each hash to print specific details. The script provides information about four different "ghosts", including their names, ages, interests, and net worth.
+
+The script defines an array of hashes named `ghosts`. Each hash contains the following keys: `:name`, `:age`, `:loves`, and `:net_worth`.
+
+The script iterates through the `ghosts` array and constructs a sentence with the information from each hash, then prints this sentence to the console.
--- carl_sagan.rb
+++ carl_sagan.rb
@@ -4,6 +4,7 @@
 #
 # We are a way for the cosmos to know itself.
 
+#1. Copy and past the following Ruby code into a file called carl_sagan.rb and follow the self-contained instructions.
 
 carl  = {
           :toast => 'cosmos',
@@ -18,5 +19,19 @@ sagan = [
           { 2 => ['to']}
         ]
 
-puts "#{carl[:words][2].capitalize} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation][1]}"
+puts "#{carl[:words][2].capitalize} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation
 
+# 2) Create an array of hashes named ‘ghosts’ to hold the following information:
+ghosts = [
+  { name: 'Inky', age: 4, loves: 'reindeers', net_worth: 25 },
+  { name: 'Pinky', age: 5, loves: 'garden tools', net_worth: 14 },
+  { name: 'Blinky', age: 7, loves: 'ninjas', net_worth: 18.03 },
+  { name: 'Clyde', age: 6, loves: 'yarn', net_worth: 0 }
+]
+
+ghosts.each do |ghost|
+  ghost_info  = "#{ghost[:name]} is #{ghost[:age]} years old, "
+  ghost_info += "loves #{ghost[:loves]} and "
+  ghost_info += "has #{'%.2f' % ghost[:net_worth]} dollars in the bank."
+  puts ghost_info
+end
